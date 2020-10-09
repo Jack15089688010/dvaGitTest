856d9263f895c9715d47661d10feb81aa087903a
c-jack.qian <c-jack.qian@dji.com>
2020-10-09 10:44:29 +0800

    fix(e): t

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -3,7 +3,7 @@ import { connect } from 'dva';
 import styles from './IndexPage.css';
 
 function IndexPage({dispatch, products, dataList}) {
-  console.log(products,"F盘",'test');
+  console.log(products,"F盘");
   return (
     <div className={styles.normal}>
       {
